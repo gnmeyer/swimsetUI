90f7009ef9a1ba7565af0c1f96f68c448ccb4716
gnmeyer <grant.meyer@liatrio.com>
2024-09-27 09:11:28 -0700

    Initial Commit

---
+++ swimgeneratorUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swimgeneratorUI
+//
+//  Created by Grant Meyer on 9/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swimgeneratorUI/swimgeneratorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  swimgeneratorUIApp.swift
+//  swimgeneratorUI
+//
+//  Created by Grant Meyer on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct swimgeneratorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
